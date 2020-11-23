a8540ee47d64d986c2b6a29546e9362e286888aa
ilya fadeev <ilya-artdesigner@yandex.ru>
2020-11-23 16:05:52 +0300

    done: #1 done: #2

--- js/input.js
+++ js/input.js
@@ -7,26 +7,26 @@ const height = document.querySelector('.height');
 const number = document.querySelector('.number');
 
 const phone_mask = Maska.create(tel, {
-  mask: '+7 (###) ### ## ##'
+  mask: '+# (###) ### ## ##'
 });
 
 const user_mask = Maska.create(user, {
-  tokens: { 'Z': { pattern: /[а-яА-Я]/ }},
+  tokens: { 'Z': { pattern: /[а-яА-Яa-zA-Z]/ }},
   mask: 'Z* Z* Z*'
 });
 
 
 const width_mask = Maska.create(width, {
   tokens: { '#': { pattern: /[0-9]/ }},
-  mask: '#### ш. см'
+  mask: '["# ш. см", "## ш. см", "### ш. см", "#### ш. см"]'
 });
 
 const height_mask = Maska.create(height, {
   tokens: { '#': { pattern: /[0-9]/ }},
-  mask: '#### в. см'
+  mask: '["# в. см", "## в. см", "### в. см", "#### в. см"]'
 });
 
 const number_mask = Maska.create(number, {
   tokens: { '#': { pattern: /[0-9]/ }},
-  mask: '#### шт'
+  mask: '["# шт", "## шт", "### шт", "#### шт"]'
 });
